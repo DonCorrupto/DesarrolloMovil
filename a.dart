b96d9d2db6a0c0da4851688bcc8fb41a95df5a94
J_Alzate <92191235+DonCorrupto@users.noreply.github.com>
2023-02-07 17:36:23 -0500

    Update a.dart

--- a.dart
+++ a.dart
@@ -1,8 +1,5 @@
 void main(List<String> args) {
-  var list = [
-    [11, 22, 54],
-    [25, 29, 35]
-  ];
+  var list = var list = [[M, S, 45], [M, S, 15], [F, S, 10], [M, C, 30], [F, S, 10], [F, C, 25], [M, C, 20]]
 
   for (var i = 0; i < list.length; i++) {
     for (var x = 0; x < list[1].length; x++) {
