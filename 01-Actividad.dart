3829829f120f8da1eb27f73f8447d0df7e63ed1e
J_Alzate <georgealzate18@hotmail.com>
2023-02-07 23:01:17 -0500

    Actividad Arreglada

--- 01-Actividad.dart
+++ 01-Actividad.dart
@@ -8,16 +8,18 @@ void main(List<String> args) {
   String input = stdin.readLineSync().toString();
 
   while (input != "0") {
-    print("1 - Ingresar un paciente, 2 - Edad promedio de hombres casados, 3 - Porcentaje de mujeres solteras respecto al total de personas solteras, 4 - Porcentaje de hombres solteros que han asistido en una semana respecto al total de hombres asistentes o 0 para salir");
+    print(
+        "1 - Ingresar un paciente, 2 - Porcentaje de hombres solteros que han asistido en una semana respecto al total de hombres asistentes, 3 - Edad promedio de hombres casados, 4 - Porcentaje de mujeres solteras respecto al total de personas solteras o 0 para salir");
     input = stdin.readLineSync().toString();
     var listTemporal = [];
     if (input == "1") {
       print("Ingrese el sexo del paciente. 1 - Hombre o 2 - Mujer");
-      var sexo = int.parse(stdin.readLineSync().toString()!);
-      print("Ingrese el estado civil del paciente, 1 - Soltero, 2 - Casado, 3 - Viudo");
-      var estadoCivil = int.parse(stdin.readLineSync().toString()!);
+      var sexo = int.parse(stdin.readLineSync().toString());
+      print(
+          "Ingrese el estado civil del paciente, 1 - Soltero, 2 - Casado, 3 - Viudo");
+      var estadoCivil = int.parse(stdin.readLineSync().toString());
       print("Ingrese la edad del paciente");
-      var edadPaciente = int.parse(stdin.readLineSync().toString()!);
+      var edadPaciente = int.parse(stdin.readLineSync().toString());
 
       listTemporal.add(sexo);
       listTemporal.add(estadoCivil);
@@ -25,59 +27,58 @@ void main(List<String> args) {
       listCentroAsistencial.add(listTemporal);
     }
 
-
     if (input == "2") {
-      var suma = 0;
-      var sumaCasado = 0;
-
+      dynamic sumaHombresSolteros = 0;
+      dynamic sumaHombresAsistentes = 0;
       for (var i = 0; i < listCentroAsistencial.length; i++) {
-        //print(listCentroAsistencial[i][1]);
-        if (listCentroAsistencial[i][0] == 1) {
-          suma += 1;
-          if (listCentroAsistencial[i][1] == 2 ) {
-            sumaCasado += int.parse(listCentroAsistencial[i][1]);
-          }
+        if (listCentroAsistencial[i][0] == "M" && listCentroAsistencial[i][1] == "S") {
+          sumaHombresSolteros += 1;
+        }
+        if (listCentroAsistencial[i][0] == "M") {
+          sumaHombresAsistentes += 1;
         }
       }
 
+      var porcentajeHombresSolterosHombresAsistentes =
+          (sumaHombresSolteros / sumaHombresAsistentes) * 100;
+      print(
+          "El porcentaje de hombres solteros que alli han asistido en una semana respecto al total de hombres asistentes $porcentajeHombresSolterosHombresAsistentes %");
+    }
+
+    if (input == "3") {
+      dynamic suma = 0;
+      dynamic sumaCasado = 0;
+
+      for (var i = 0; i < listCentroAsistencial.length; i++) {
+        if (listCentroAsistencial[i][1] == "C" &&
+            listCentroAsistencial[i][0] == "M") {
+          sumaCasado += listCentroAsistencial[i][2];
+          suma += 1;
+        }
+      }
       var promedioCasado = sumaCasado / suma;
       print("El promedio de edad de hombres casados son $promedioCasado");
     }
 
-    if (input == "3") {
-      var sumaMujerSoltera = 0;
-      var sumaPersonaSolteras = 0;
+    if (input == "4") {
+      dynamic sumaMujerSoltera = 0;
+      dynamic sumaPersonaSolteras = 0;
       for (var i = 0; i < listCentroAsistencial.length; i++) {
-        if (listCentroAsistencial[i][0] == 2 && listCentroAsistencial[i][1] == 1) {
+        if (listCentroAsistencial[i][0] == "F" &&
+            listCentroAsistencial[i][1] == "S") {
           sumaMujerSoltera += 1;
         }
 
-        if (listCentroAsistencial[i][1] == 1) {
+        if (listCentroAsistencial[i][1] == "S") {
           sumaPersonaSolteras += 1;
         }
       }
 
-      var porcentajeMujeresSolteraspersonasSolteras = (sumaMujerSoltera/sumaPersonaSolteras)*100;
+      var porcentajeMujeresSolteraspersonasSolteras =
+          (sumaMujerSoltera / sumaPersonaSolteras) * 100;
 
-      print("El porcentaje de mujeres solteras respecto al total de personas solteras son $porcentajeMujeresSolteraspersonasSolteras %");
+      print(
+          "El porcentaje de mujeres solteras respecto al total de personas solteras son $porcentajeMujeresSolteraspersonasSolteras %");
     }
-
-    if (input == "4") {
-      var sumaHombresSolteros = 0;
-      var sumaHombresAsistentes = 0;
-      for (var i = 0; i < listCentroAsistencial.length; i++) {
-        if (listCentroAsistencial[i][0] == 1 && listCentroAsistencial[i][1] == 1) {
-          sumaHombresSolteros += 1;
-        }
-        if (listCentroAsistencial[i][0] == 1) {
-          sumaHombresAsistentes += 1;
-        }
-
-      }
-
-      var porcentajeHombresSolterosHombresAsistentes = (sumaHombresSolteros/sumaHombresAsistentes)*100;
-      print("El porcentaje de hombres solteros que alli han asistido en una semana respecto al total de hombres asistentes $porcentajeHombresSolterosHombresAsistentes %");
-    }
-
   }
 }
--- a.dart
+++ a.dart
@@ -1,10 +1,20 @@
 void main(List<String> args) {
-  var list = var list = [[M, S, 45], [M, S, 15], [F, S, 10], [M, C, 30], [F, S, 10], [F, C, 25], [M, C, 20]]
+  var list = [
+    ["M", "S", 45],
+    ["M", "S", 15],
+    ["F", "S", 10],
+    ["M", "C", 30],
+    ["F", "S", 10],
+    ["F", "C", 25],
+    ["M", "C", 20]
+  ];
 
+  /*
   for (var i = 0; i < list.length; i++) {
     for (var x = 0; x < list[1].length; x++) {
       //print(list[i][x]);
     }
     print(list[i][0]);
   }
+  */
 }
