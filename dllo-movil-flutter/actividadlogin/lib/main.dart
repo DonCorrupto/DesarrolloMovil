2252f67c7b8728c3579d6c9d0831b29ec2c50ae8
DonCorrupto <georgealzate18@hotmail.com>
2023-05-02 16:49:30 -0500

    Actividad Login

---
+++ dllo-movil-flutter/actividadlogin/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:actividadlogin/pages/login.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home:  Login(),
+      //  Scaffold(
+      //     appBar: AppBar(
+      //       title: const Text("To Do"),
+      //       titleSpacing: 100,
+      //     ),
+      //     body:ButtonBase(),
+
+      //  )
+    );
+  }
+}
